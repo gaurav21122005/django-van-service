1e225a1ac4b593b58b05a411b08bd879d767f01a
gaurav21122005 <manegaurav229@gmail.com>
2026-04-13 11:17:49 +0530

    Update wsgi.py

--- vanservice/wsgi.py
+++ vanservice/wsgi.py
@@ -1,6 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'van_service_project.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vanservice.settings')
 application = get_wsgi_application()
